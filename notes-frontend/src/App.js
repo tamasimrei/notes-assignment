41f68156ccd4ce02869bfd5a7e198b7397400a8c
Tom Imrei <timrei@tmhcc.com>
2022-08-09 16:41:06 -0700

    Creating frontend app

--- notes-frontend/src/App.js
+++ notes-frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import {Nav, Container, Row, Col, Button} from 'react-bootstrap'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return(
+        <>
+            <BrowserRouter>
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link as={Link} to={"/"}>Notes</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link as={Link} to={"/tags"}>Tags</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <Container fluid>
+                    <Row>
+                        <Col>
+                            <Routes>
+                                <Route exact path="/" element={<Notes />}/>
+                                <Route exact path="/tags" element={<Tags />}/>
+                            </Routes>
+                        </Col>
+                    </Row>
+                </Container>
+            </BrowserRouter>
+        </>
+    )
 }
 
-export default App;
+function Notes() {
+    return(
+        <>
+            <h2>Notes</h2>
+            <Button>Foo</Button>
+        </>
+    )
+}
+
+function Tags() {
+    return(
+        <>
+            <h2>Tags</h2>
+            <Button>Bar</Button>
+        </>
+    )
+}
--- notes-frontend/src/App.test.js
+++ notes-frontend/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react'
+// import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Disabled test created by create-react-app
+// test('renders learn react link', () => {
+//   render(<App />)
+//   const linkElement = screen.getByText(/learn react/i)
+//   expect(linkElement).toBeInTheDocument()
+// })
