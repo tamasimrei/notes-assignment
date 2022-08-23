9c4d2fa37ed441612d33a83609ad54c6de436ed9
Tom Imrei <timrei@tmhcc.com>
2022-08-23 16:13:56 -0700

    Remove id from tag badge

--- notes-frontend/src/Notes/NoteTag.js
+++ notes-frontend/src/Notes/NoteTag.js
@@ -3,6 +3,6 @@ import {Badge} from "react-bootstrap";
 
 export default function NoteTag(props) {
     return (
-        <Badge id={"tag" + props.tag.id} className="me-2 px-3 py-2">{props.tag.name}</Badge>
+        <Badge className="me-2 px-3 py-2">{props.tag.name}</Badge>
     )
 }
